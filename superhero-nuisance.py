bbfb95dc82bdd54c46746d1b6ae617b0abdc75bb
Codeaday <AJORIGAMI1@gmail.com>
2019-05-24 03:21:42 -0500

    Add files via upload

---
+++ superhero-nuisance.py
@@ -0,0 +1,24 @@
+# Write a Python procedure fix_machine to take 2 string inputs
+# and returns the 2nd input string as the output if all of its
+# characters can be found in the 1st input string and "Give me
+# something that's not useless next time." if it's impossible.
+# Letters that are present in the 1st input string may be used
+# as many times as necessary to create the 2nd string (you
+# don't need to keep track of repeat usage).
+
+
+def fix_machine(debris, product):
+    p = 0
+    d = 0
+    while p < len(product):
+        if debris.find(product[p]) != -1:
+            p = p + 1
+        else:
+            return "Give me something that's not useless next time."
+    return product
+
+
+print("Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me something that's not useless next time.")
+print("Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity')
+print("Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity')
+print("Test case 4: ", fix_machine('wsx0-=mttrhix', 't-shirt') == 't-shirt')
